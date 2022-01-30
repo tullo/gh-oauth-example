52278c528deaddb66599f06e555746c81dbfc678
Andreas Amstutz <tullo@users.noreply.github.com>
2022-01-30 21:42:11 +0100

    oauth redirect: exchange code for  access token

--- main.go
+++ main.go
@@ -1,12 +1,53 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
+
+	http.HandleFunc("/oauth/redirect", func(w http.ResponseWriter, r *http.Request) {
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(os.Stdout, "could not parse query: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		code := r.FormValue("code")
+
+		// retrieve access token
+		const clientID = "73b2ad007cdc9bf3dd09"
+		const clientSecret = ""
+		url := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", clientID, clientSecret, code)
+		req, err := http.NewRequest(http.MethodPost, url, nil)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not create HTTP request: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		req.Header.Set("accept", "application/json")
+
+		var c http.Client
+		res, err := c.Do(req)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not send HTTP request: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		defer res.Body.Close()
+
+		var t struct {
+			AccessToken string `json:"access_token"`
+		}
+		if err := json.NewDecoder(res.Body).Decode(&t); err != nil {
+			fmt.Fprintf(os.Stdout, "could not parse JSON response: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		w.Header().Set("Location", "/welcome.html?access_token="+t.AccessToken)
+		w.WriteHeader(http.StatusFound)
+	})
 
 	http.ListenAndServe(":8080", nil)
 }
